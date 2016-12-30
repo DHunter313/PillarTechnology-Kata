11113deecd936cad4b9204d602167b53f1434a85
Darius Hunter <darius777hunter@gmail.com>
2016-12-30 10:40:40 -0500

    Added a txt file with the instructions from the customer. added a third test for after bedtime wage. test failed. need to refactor.

--- PillarTechnology/Kata_BabySitterPayCalculator/Kata_BabySitterPayCalculator/UnitTest1.cs
+++ PillarTechnology/Kata_BabySitterPayCalculator/Kata_BabySitterPayCalculator/UnitTest1.cs
@@ -23,5 +23,17 @@ namespace PayCalculatorTestCases
 
             Assert.AreEqual(60, pay);
         }
+
+        [TestMethod]
+        public void Wage_AFterBedtime()
+        {
+            //5 hours worked
+            // hours from start time to bedTime. example startTime is 7pm, and bedtime at 9pm. Value would be 2. 
+            // pay equals $42
+
+            int pay = Program.WageCalc(5);
+
+            Assert.AreEqual(44, pay);
+        }
     }
 }
