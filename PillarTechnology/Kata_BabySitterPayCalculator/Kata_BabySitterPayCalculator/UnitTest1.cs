433202535f27a8f5c9c87e949f6ca7cd12d0923d
Darius Hunter <darius777hunter@gmail.com>
2016-12-30 10:59:26 -0500

    Wage_AFterBedtime test passed after refactoring.

--- PillarTechnology/Kata_BabySitterPayCalculator/Kata_BabySitterPayCalculator/UnitTest1.cs
+++ PillarTechnology/Kata_BabySitterPayCalculator/Kata_BabySitterPayCalculator/UnitTest1.cs
@@ -14,26 +14,31 @@ namespace PayCalculatorTestCases
         //    Assert.AreEqual(0, pay);
         //}
 
-        [TestMethod]
-        public void WageTest_BeforeBedtime()
-        {
-            // 5 hours worked at a rate of 12/hr = 60
+        //[TestMethod]
+        //public void WageTest_BeforeBedtime()
+        //{
+        //    // 5 hours worked at a rate of 12/hr = 60
 
-            int pay = Program.WageCalc(5);
+        //    int pay = Program.WageCalc(5);
 
-            Assert.AreEqual(60, pay);
-        }
+        //    Assert.AreEqual(60, pay);
+        //}
 
         [TestMethod]
         public void Wage_AFterBedtime()
         {
             //5 hours worked
             // hours from start time to bedTime. example startTime is 7pm, and bedtime at 9pm. Value would be 2. 
-            // pay equals $42
+            // pay equals $48
 
-            int pay = Program.WageCalc(5);
+            int pay1 = Program.WageCalc(5,2);
+            Assert.AreEqual(48, pay1);
 
-            Assert.AreEqual(44, pay);
+            // 2 hour shift, 1 hour worked before bedtime. 
+            // pay should equal  20
+            int pay2 = Program.WageCalc(2, 1);
+            Assert.AreEqual(20, pay2);
+
         }
     }
 }
