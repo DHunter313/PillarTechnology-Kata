f09eb3c55104890b2ace89ff2883d39d51a8e23b
Darius Hunter <darius777hunter@gmail.com>
2016-12-30 11:15:46 -0500

    WageTest_AfterMidnite test has passed after the refactoring.

--- PillarTechnology/Kata_BabySitterPayCalculator/Kata_BabySitterPayCalculator/UnitTest1.cs
+++ PillarTechnology/Kata_BabySitterPayCalculator/Kata_BabySitterPayCalculator/UnitTest1.cs
@@ -24,35 +24,35 @@ namespace PayCalculatorTestCases
         //    Assert.AreEqual(60, pay);
         //}
 
-        [TestMethod]
-        public void Wage_AFterBedtime()
-        {
-            //5 hours worked
-            // hours from start time to bedTime. example startTime is 7pm, and bedtime at 9pm. Value would be 2. 
-            // pay equals $48
+        //[TestMethod]
+        //public void Wage_AFterBedtime()
+        //{
+        //    //5 hours worked
+        //    // hours from start time to bedTime. example startTime is 7pm, and bedtime at 9pm. Value would be 2. 
+        //    // pay equals $48
 
-            int pay1 = Program.WageCalc(5,2);
-            Assert.AreEqual(48, pay1);
+        //    int pay1 = Program.WageCalc(5,2);
+        //    Assert.AreEqual(48, pay1);
 
-            // 2 hour shift, 1 hour worked before bedtime. 
-            // pay should equal  20
-            int pay2 = Program.WageCalc(2, 1);
-            Assert.AreEqual(20, pay2);
+        //    // 2 hour shift, 1 hour worked before bedtime. 
+        //    // pay should equal  20
+        //    int pay2 = Program.WageCalc(2, 1);
+        //    Assert.AreEqual(20, pay2);
 
-        }
+        //}
 
-        [TestMethod]
-        public void AfterBedtime_BeforeMidnite()
-        {
-            // 3 hours of work
-            // started at 7pm, and ends at 10pm.
-            // bedtime is 8pm, which is 1 hour after start time.
-            // Wage should equal 28
+        //[TestMethod]
+        //public void AfterBedtime_BeforeMidnite()
+        //{
+        //    // 3 hours of work
+        //    // started at 7pm, and ends at 10pm.
+        //    // bedtime is 8pm, which is 1 hour after start time.
+        //    // Wage should equal 28
 
-            double pay = Program.WageCalc(3, 1);
+        //    double pay = Program.WageCalc(3, 1);
 
-            Assert.AreEqual(28, pay);
-        }
+        //    Assert.AreEqual(28, pay);
+        //}
 
         [TestMethod]
         public void WageTest_AfterMidnite()
@@ -60,9 +60,10 @@ namespace PayCalculatorTestCases
             // totalTime is 10 hours
             // start time is 5pm
             // bedtime is 11pm, which is 6 hours from start time
+            // that equals 1 hour of bedtime before midnite, and after midnite hours = 3
             //wage should equal $128
-            //
-            int pay = Program.WageCalc(10, 6);
+            
+            int pay = Program.WageCalc(10, 6, 3);
 
             Assert.AreEqual(128, pay);
         }
