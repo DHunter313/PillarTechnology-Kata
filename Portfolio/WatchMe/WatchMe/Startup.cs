97e272fca5add22103d55a17db9bf4d8b0d15eae
dhunter313 <dhunter18@hawkmail.hfcc.edu>
2017-02-15 18:41:30 -0500

    Created project added a  Movie class.

---
+++ Portfolio/WatchMe/WatchMe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WatchMe.Startup))]
+namespace WatchMe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
